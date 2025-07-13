11bf75a85ba7decf794a26593ed42c815acf671e
Xecuc <Kostyaleschenko090807@gmail.com>
2025-07-14 02:19:00 +0300

    feat: Add asset and structure for day/night

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ GRAVITY = 0.75
 JUMP_STRENGTH = -12
 PIPE_GAP = 300
 PIPE_FREQUENCY = 1500 # milliseconds
+SCROLL_SPEED = 6 # Speed for the moving ground
 
 # --- Asset Paths & Configs ---
 FONT_PATH = 'assets/04B_19.TTF'
@@ -24,7 +25,17 @@ ASSETS = {
         'jump': 'assets/wing.wav',
         'hit': 'assets/hit.wav',
         'score': 'assets/point.wav',
+    },
+    'themes': {
+        'day': {
+            'background': 'assets/bg.png',
+            'pipe': 'assets/pipe.png',
+            'ground': 'assets/ground.png'
+        },
+        'night': {
+            'background': 'assets/bg-night.png',
+            'pipe': 'assets/pipe-night.png',
+            'ground': 'assets/ground-night.png'
+        }
     }
 }
-
-
