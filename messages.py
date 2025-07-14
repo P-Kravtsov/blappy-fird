90ce6debe0c63167af8a2cc0573b7d6d09a7b3b8
Lodems <nazarminich07@gmail.com>
2025-07-14 19:11:27 +0300

    Add random death messages when game over

---
+++ messages.py
@@ -0,0 +1,49 @@
+DEATH_MESSAGES = [
+    # English - Mocking
+    "Not even close.",
+    "The pipes send their regards.",
+    "Was that a bird or a brick?",
+    "Maybe try flapping?",
+    "The ground seemed very appealing, huh?",
+    "You're breathtaking...ly bad at this.",
+    "Task failed successfully.",
+    "That was... unexpected.",
+    "Do a barrel roll! Oh, wait.",
+    "It's just a game. A game you're losing.",
+
+    # Polish - Mocking
+    "Grawitacja znowu wygrała.",
+    "Rury przesyłają pozdrowienia.",
+    "Nawet nie było blisko.",
+    "Chyba ptak, chyba kamień.",
+    "Ziemia była zbyt kusząca?",
+    "Moja babcia gra lepiej.",
+    "Przynajmniej próbowałeś. Chyba.",
+    "To było... nieoczekiwane.",
+    "Czy to miało tak wyglądać?",
+    "Koniec gry. I co teraz?",
+
+    # English - Supportive
+    "You can do better!",
+    "Every fall is a lesson.",
+    "Don't give up!",
+    "One more try!",
+    "You'll get it next time!",
+    "Practice makes perfect.",
+    "Keep your head up.",
+    "Failure is the condiment that gives success its flavor.",
+    "The sky is the limit!",
+    "You got this!",
+
+    # Polish - Supportive
+    "Dasz radę!",
+    "Jeszcze raz!",
+    "Nie poddawaj się!",
+    "Następnym razem się uda!",
+    "Praktyka czyni mistrza.",
+    "Każdy upadek to lekcja.",
+    "Głowa do góry.",
+    "Prawie się udało!",
+    "Jesteś coraz lepszy.",
+    "Spróbujmy jeszcze raz!",
+]
